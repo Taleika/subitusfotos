c97805e52a3dd21f0e944e83479f40ab42d90791
Taleika Films <ferpache8@gmail.com>
2026-08-20 14:00:57 -0300

    Add reusable event configuration

---
+++ event-config.js
@@ -0,0 +1,10 @@
+export const EVENT_CONFIG = {
+  id: "evento-activo",
+  eyebrow: "Compartí la fiesta",
+  title: "Subí tus fotos",
+  description:
+    "Tus fotos pueden formar parte del video que Taleika está creando durante la fiesta.",
+  uploadEnabled: true,
+  maxFileSizeMb: 30,
+  minLongEdgePx: 1920,
+};
